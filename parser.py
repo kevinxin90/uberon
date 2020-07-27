593f5b2a78e2083ace243d41433a9fdcdf74ce6c
jiwen xin <kevinxin@scripps.edu>
2020-07-27 10:35:28 -0700

    update parser to beautify xref, relationship structure

--- parser.py
+++ parser.py
@@ -1,25 +1,45 @@
+from collections import defaultdict
 import networkx as nx
 import obonet
 import os
 
 def load_data(data_folder):
-    path = os.path.join(data_folder, "ext.obo")
+    path = os.path.join(data_folder, "uberon.obo")
     graph = obonet.read_obo(path)
     for item in graph.nodes():
         if item.startswith("UBERON:"):
             rec = graph.nodes[item]
             rec["_id"] = item
             if rec.get("is_a"):
-                rec["parents"] = rec.pop("is_a")
-            if rec.get("xrefs"):
-                rec["xrefs"] = rec.pop('xref')
-            rec["children"] = list(graph.predecessors(item))
-            rec["ancestors"] = list(nx.descendants(graph, item))
-            rec["descendants"] = list(nx.ancestors(graph,item))
+                rec["parents"] = [parent for parent in rec.pop("is_a") if parent.startswith("UBERON:")]
+            if rec.get("xref"):
+                xrefs = defaultdict(set)
+                for val in rec.get("xref"):
+                    if ":" in val:
+                        prefix, id = val.split(':', 1)
+                        if prefix in ["http", "https"]:
+                            continue
+                        if prefix in ['UMLS', 'MESH']:
+                            xrefs[prefix.lower()].add(id)
+                        else:
+                            xrefs[prefix.lower()].add(val)
+                for k, v in xrefs.items():
+                    xrefs[k] = list(v)
+                rec.pop("xref")
+                rec["xrefs"] = dict(xrefs)
+            rec["children"] = [child for child in graph.predecessors(item) if child.startswith("UBERON:")]
+            rec["ancestors"] = [ancestor for ancestor in nx.descendants(graph, item) if ancestor.startswith("UBERON:")]
+            rec["descendants"] = [descendant for descendant in nx.ancestors(graph,item) if descendant.startswith("UBERON:")]
             if rec.get("created_by"):
                 rec.pop("created_by")
             if rec.get("creation_date"):
                 rec.pop("creation_date")
             if rec.get("property_value"):
                 rec.pop("property_value")
+            if rec.get("relationship"):
+                for rel in rec.get("relationship"):
+                    predicate, val = rel.split(' ')
+                    prefix = val.split(':')[0]
+                    rec[predicate] = {prefix.lower(): val}
+                rec.pop("relationship")
             yield rec
