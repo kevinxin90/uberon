034c69d43ac6f7aa00025abc685d489448388e66
jiwen xin <kevinxin@scripps.edu>
2020-07-27 14:47:32 -0700

    update rel

--- parser.py
+++ parser.py
@@ -37,9 +37,18 @@ def load_data(data_folder):
             if rec.get("property_value"):
                 rec.pop("property_value")
             if rec.get("relationship"):
+                rels = {}
                 for rel in rec.get("relationship"):
                     predicate, val = rel.split(' ')
                     prefix = val.split(':')[0]
-                    rec[predicate] = {prefix.lower(): val}
+                    if predicate not in rels:
+                        rels[predicate] = defaultdict(set)
+                    if prefix.lower() not in rels[predicate]:
+                        rels[predicate][prefix.lower()].add(val)
+                for m, n in rels.items():
+                    for p, q in n.items():
+                        n[p] = list(q)
+                    rels[m] = dict(n)
+                rec.update(rels)
                 rec.pop("relationship")
             yield rec
